74efd4c654d507a7d43e4013688c321229ab36dc
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-03-01 16:03:35 -0800

    initial commit

---
+++ OCWeatheringMood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCWeatheringMood
+//
+//  Created by David Anglin on 3/1/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCWeatheringMood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCWeatheringMood
+//
+//  Created by David Anglin on 3/1/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
